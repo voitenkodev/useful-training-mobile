7967c4b03c1d6545c636df910d79de752cc98c81
maxvoitenko <voitenko.dev@gmail.com>
2022-10-11 19:29:38 +0300

    clear odd

--- app/src/commonMain/kotlin/platform.kt
+++ app/src/commonMain/kotlin/platform.kt
@@ -1,12 +1,5 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import data.source.AuthProtocol
 import data.source.TrainingProtocol
-import org.koin.core.module.Module
-import kotlin.coroutines.CoroutineContext
-
-
-
 
 expect class AuthSource() : AuthProtocol
 expect class TrainingSource() : TrainingProtocol
