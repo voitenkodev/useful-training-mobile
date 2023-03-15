f0afe81ea88bdb7d34b5aacfb8f02a1b1a6ad0d5
maxvoitenko <voitenko.dev@gmail.com>
2023-03-16 00:16:22 +0200

    fix paddings

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,6 +1,18 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Application
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController = Application("UsefulTraining") {
-    Main { }
+    Column {
+        Box(modifier = Modifier.fillMaxWidth().height(44.dp).background(Color.Black))
+        Main { }
+        Box(modifier = Modifier.fillMaxWidth().height(44.dp).background(Color.Black))
+    }
 }
