73d43d4cdd88601e0ed6fbe2bf98716e122a123c
Maxvoitenko <voitenko.dev@gmail.com>
2023-07-25 15:05:08 +0300

    add clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,3 +19,16 @@ subprojects {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
+
+tasks.register("clean", Delete::class) {
+    delete(".gradle")
+    delete(".idea")
+    delete("build")
+
+    delete("androidApp/build")
+    delete("data/build")
+    delete("shared/build")
+    delete("utils/instruments/build")
+    delete("utils/navigator/build")
+}
