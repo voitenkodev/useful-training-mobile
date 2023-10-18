a92c0ae13e984e2de1e0a1e902ade1c5a66fb4d2
Maxvoitenko <voitenko.dev@gmail.com>
2023-10-19 00:55:13 +0300

    clear modules + fix script

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -6,7 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        DiKt.doInitCommonKoin { Koin_coreKoinApplication in }
+        DiKt.doInitCommonKoin { Koin_coreKoinApplication in
+            
+        }
         window = UIWindow(frame: UIScreen.main.bounds)
         let mainViewController = Main_iosKt.main()
         window?.rootViewController = mainViewController
