96f6147eda7b309686df81f81f73017e93961826
Maxvoitenko <voitenko.dev@gmail.com>
2023-10-27 00:01:35 +0300

    fix database crash

--- services/database/src/commonMain/kotlin/traininig_exercise_iteration/TrainingsSource.kt
+++ services/database/src/commonMain/kotlin/traininig_exercise_iteration/TrainingsSource.kt
@@ -4,12 +4,12 @@ import NativeContext
 import UsefulTrainingDatabase
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
+import app.cash.sqldelight.coroutines.mapToOne
 import data.Exercise
 import data.Iteration
 import database
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import traininig_exercise_iteration.mapping.toDao
 import traininig_exercise_iteration.models.ExerciseDao
@@ -22,40 +22,31 @@ public class TrainingsSource(nativeContext: NativeContext) {
     private val api by lazy { database.training_exercise_iterationQueries }
 
     public fun getTrainings(): Flow<List<TrainingDao>> {
-
-        val result = api
+        return api
             .getTrainings()
             .asFlow()
             .mapToList(Dispatchers.Default)
             .map { trainings ->
                 trainings.map { trainingEntity ->
-
                     val exercises = getExercisesBy {
-                        api
-                            .getExercisesByTrainingId(trainingEntity.id)
-                            .executeAsList()
+                        api.getExercisesByTrainingId(trainingEntity.id).executeAsList()
                     }
-
                     trainingEntity.toDao(exercises = exercises)
                 }
             }
-
-        return result
     }
 
     public fun getTraining(trainingId: String): Flow<TrainingDao> {
-
-        val result = api
+        return api
             .getTrainingById(trainingId)
-            .executeAsOne()
-
-        val dao = getExercisesBy {
-            api
-                .getExercisesByTrainingId(result.id)
-                .executeAsList()
-        }
-
-        return flowOf(result.toDao(dao))
+            .asFlow()
+            .mapToOne(Dispatchers.Default)
+            .map { result ->
+                val exercises = getExercisesBy {
+                    api.getExercisesByTrainingId(result.id).executeAsList()
+                }
+                result.toDao(exercises)
+            }
     }
 
     public fun setTrainings(trainings: List<TrainingDao>) {
@@ -116,20 +107,17 @@ public class TrainingsSource(nativeContext: NativeContext) {
         api.deleteTrainingById(id = trainingId)
     }
 
-    private fun getExercisesBy(action: () -> List<Exercise>): List<ExerciseDao> {
-        return action().map { exerciseEntity ->
+    private fun getExercisesBy(query: () -> List<Exercise>): List<ExerciseDao> {
+        return query().map { exerciseEntity ->
             val iterations = getIterationsBy {
-                api
-                    .getIterationsByExercisesId(exerciseEntity.id)
-                    .executeAsList()
+                api.getIterationsByExercisesId(exerciseEntity.id).executeAsList()
             }
-
             exerciseEntity.toDao(iterations = iterations)
         }
     }
 
-    private fun getIterationsBy(action: () -> List<Iteration>): List<IterationDao> {
-        return action().map { iterationEntity ->
+    private fun getIterationsBy(query: () -> List<Iteration>): List<IterationDao> {
+        return query().map { iterationEntity ->
             iterationEntity.toDao()
         }
     }
