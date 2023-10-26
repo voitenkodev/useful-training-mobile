e00e24294e073aa0a6e49d6a71c6a5914050b143
Maxvoitenko <voitenko.dev@gmail.com>
2023-10-26 17:56:49 +0300

    upd version

--- services/database/src/iosMain/kotlin/PlatformDatabase.kt
+++ services/database/src/iosMain/kotlin/PlatformDatabase.kt
@@ -1,15 +1,6 @@
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import app.cash.sqldelight.driver.native.wrapConnection
-import co.touchlab.sqliter.DatabaseConfiguration
 
 internal actual fun NativeContext.database(): UsefulTrainingDatabase {
-    val dbConfig = DatabaseConfiguration(
-        name = DB_FILE_NAME,
-        version = UsefulTrainingDatabase.Schema.version,
-        extendedConfig = DatabaseConfiguration.Extended(foreignKeyConstraints = true),
-        create = { connection ->
-            wrapConnection(connection) { UsefulTrainingDatabase.Schema.create(it) }
-        }
-    )
-    return UsefulTrainingDatabase.invoke(NativeSqliteDriver(dbConfig))
+    val driver = NativeSqliteDriver(UsefulTrainingDatabase.Schema, DB_FILE_NAME)
+    return UsefulTrainingDatabase(driver)
 }
