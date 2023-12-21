3828bbf057cb139fd92c0fd91922caec13a77dd4
maxvoitenko <voitenko.dev@gmail.com>
2023-12-22 01:33:37 +0200

    migrate api muscles domain

--- domain/exercise-examples/src/commonMain/kotlin/ExerciseExamplesRepository.kt
+++ domain/exercise-examples/src/commonMain/kotlin/ExerciseExamplesRepository.kt
@@ -2,8 +2,6 @@ import kotlinx.coroutines.flow.Flow
 import models.ExerciseExample
 
 public interface ExerciseExamplesRepository {
-
-    // Exercise examples
     public fun observeExerciseExamples(): Flow<List<ExerciseExample>>
     public fun syncExerciseExamples(): Flow<Unit>
     public fun observeExerciseExample(exerciseExampleId: String): Flow<ExerciseExample>
