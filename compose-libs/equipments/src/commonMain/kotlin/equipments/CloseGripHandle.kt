1f29553df72a1c6922016cac2acccf404ee50442
maxvoitenko <maxim.voitenko@planetvpn.dev>
2024-09-07 00:43:02 +0300

    upd grips

--- compose-libs/equipments/src/commonMain/kotlin/equipments/CloseGripHandle.kt
+++ compose-libs/equipments/src/commonMain/kotlin/equipments/CloseGripHandle.kt
@@ -8,24 +8,24 @@ import androidx.compose.ui.unit.dp
 
 public val CloseGripHandle: ImageVector
     get() {
-        if (_Closegriphandle != null) {
-            return _Closegriphandle!!
+        if (_CloseGripHandle != null) {
+            return _CloseGripHandle!!
         }
-        _Closegriphandle = ImageVector.Builder(
-            name = "Closegriphandle",
-            defaultWidth = 106.dp,
-            defaultHeight = 100.dp,
-            viewportWidth = 106f,
-            viewportHeight = 100f
+        _CloseGripHandle = ImageVector.Builder(
+            name = "CloseGripHandle",
+            defaultWidth = 142.dp,
+            defaultHeight = 148.dp,
+            viewportWidth = 142f,
+            viewportHeight = 148f
         ).apply {
             path(fill = SolidColor(Color(0xFFD9D9D9))) {
-                moveTo(73.3f, 46.51f)
-                lineTo(14.97f, 66.71f)
-                curveTo(14.97f, 66.71f, 8.32f, 67.25f, 6.06f, 59.74f)
-                curveTo(3.81f, 52.24f, 11.05f, 48.52f, 11.05f, 48.52f)
-                lineTo(69.75f, 33.21f)
-                curveTo(69.75f, 33.21f, 74.49f, 32.83f, 75.99f, 37.77f)
-                curveTo(77.7f, 43.4f, 73.3f, 46.51f, 73.3f, 46.51f)
+                moveTo(91.3f, 70.51f)
+                lineTo(32.97f, 90.71f)
+                curveTo(32.97f, 90.71f, 26.32f, 91.25f, 24.06f, 83.74f)
+                curveTo(21.81f, 76.24f, 29.05f, 72.52f, 29.05f, 72.52f)
+                lineTo(87.75f, 57.21f)
+                curveTo(87.75f, 57.21f, 92.49f, 56.83f, 93.99f, 61.77f)
+                curveTo(95.7f, 67.4f, 91.3f, 70.51f, 91.3f, 70.51f)
                 close()
             }
             path(
@@ -33,89 +33,89 @@ public val CloseGripHandle: ImageVector
                 stroke = SolidColor(Color(0xFFAAAAAA)),
                 strokeLineWidth = 1f
             ) {
-                moveTo(27.27f, 61.94f)
-                curveTo(29.88f, 56.26f, 27.08f, 47.02f, 23.32f, 46.15f)
-                lineTo(59.8f, 36.44f)
-                curveTo(65.03f, 40.38f, 66.11f, 45.02f, 64.57f, 49.17f)
-                lineTo(27.27f, 61.94f)
+                moveTo(45.27f, 85.94f)
+                curveTo(47.88f, 80.26f, 45.08f, 71.02f, 41.32f, 70.15f)
+                lineTo(77.8f, 60.44f)
+                curveTo(83.03f, 64.38f, 84.11f, 69.02f, 82.57f, 73.17f)
+                lineTo(45.27f, 85.94f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFA0A0A0))) {
-                moveTo(16.57f, 56.31f)
-                curveTo(18.3f, 59.71f, 17.7f, 63.41f, 15.24f, 64.58f)
-                curveTo(12.78f, 65.75f, 9.39f, 63.94f, 7.66f, 60.53f)
-                curveTo(5.93f, 57.13f, 6.52f, 53.43f, 8.98f, 52.26f)
-                curveTo(11.44f, 51.1f, 14.84f, 52.91f, 16.57f, 56.31f)
+                moveTo(34.57f, 80.31f)
+                curveTo(36.3f, 83.71f, 35.7f, 87.41f, 33.24f, 88.58f)
+                curveTo(30.78f, 89.75f, 27.39f, 87.94f, 25.66f, 84.53f)
+                curveTo(23.93f, 81.13f, 24.52f, 77.43f, 26.98f, 76.26f)
+                curveTo(29.44f, 75.1f, 32.84f, 76.91f, 34.57f, 80.31f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFBEBEBE))) {
-                moveTo(61.99f, 35.5f)
-                lineTo(70.8f, 15.32f)
-                curveTo(72.26f, 15.95f, 74.62f, 17.97f, 74.62f, 17.97f)
-                lineTo(65.9f, 37.32f)
-                curveTo(65.9f, 37.32f, 65.38f, 37.95f, 63.51f, 37.32f)
-                curveTo(62.43f, 36.95f, 61.99f, 35.5f, 61.99f, 35.5f)
+                moveTo(79.99f, 59.5f)
+                lineTo(88.8f, 39.32f)
+                curveTo(90.26f, 39.95f, 92.62f, 41.97f, 92.62f, 41.97f)
+                lineTo(83.9f, 61.32f)
+                curveTo(83.9f, 61.32f, 83.38f, 61.95f, 81.51f, 61.32f)
+                curveTo(80.43f, 60.95f, 79.99f, 59.5f, 79.99f, 59.5f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFBEBEBE))) {
-                moveTo(17.71f, 47.95f)
-                lineTo(25.16f, 29.14f)
-                curveTo(26.64f, 29.58f, 28.16f, 29.72f, 28.16f, 29.72f)
-                lineTo(21.97f, 49.68f)
-                curveTo(21.97f, 49.68f, 21.54f, 50.36f, 19.68f, 49.97f)
-                curveTo(18.6f, 49.74f, 17.71f, 47.95f, 17.71f, 47.95f)
+                moveTo(35.71f, 71.95f)
+                lineTo(43.16f, 53.14f)
+                curveTo(44.64f, 53.58f, 46.16f, 53.72f, 46.16f, 53.72f)
+                lineTo(39.97f, 73.68f)
+                curveTo(39.97f, 73.68f, 39.54f, 74.36f, 37.68f, 73.97f)
+                curveTo(36.6f, 73.74f, 35.71f, 71.95f, 35.71f, 71.95f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFA0A0A0))) {
-                moveTo(23.81f, 20.24f)
-                curveTo(32.31f, 9.25f, 48.22f, 6.08f, 48.22f, 6.08f)
-                lineTo(49.2f, 13.24f)
-                curveTo(49.2f, 13.24f, 45.84f, 14.15f, 46.34f, 16.63f)
-                curveTo(46.93f, 19.61f, 50.77f, 18.85f, 50.77f, 18.85f)
-                lineTo(53.77f, 28.42f)
-                lineTo(24.78f, 34.21f)
-                curveTo(24.78f, 34.21f, 13.38f, 33.73f, 23.81f, 20.24f)
+                moveTo(41.81f, 44.24f)
+                curveTo(50.31f, 33.25f, 66.22f, 30.08f, 66.22f, 30.08f)
+                lineTo(67.2f, 37.24f)
+                curveTo(67.2f, 37.24f, 63.84f, 38.15f, 64.34f, 40.63f)
+                curveTo(64.93f, 43.61f, 68.77f, 42.85f, 68.77f, 42.85f)
+                lineTo(71.77f, 52.42f)
+                lineTo(42.78f, 58.21f)
+                curveTo(42.78f, 58.21f, 31.38f, 57.73f, 41.81f, 44.24f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFA0A0A0))) {
-                moveTo(73.96f, 10.24f)
-                curveTo(58.89f, 3.95f, 46.38f, 6.45f, 46.38f, 6.45f)
-                lineTo(48.57f, 13.37f)
-                curveTo(48.57f, 13.37f, 51.58f, 12.77f, 52.12f, 15.48f)
-                curveTo(52.71f, 18.46f, 48.93f, 19.21f, 48.93f, 19.21f)
-                lineTo(49.85f, 29.21f)
-                lineTo(78.39f, 23.51f)
-                curveTo(78.39f, 23.51f, 89.37f, 16.67f, 73.96f, 10.24f)
+                moveTo(91.96f, 34.24f)
+                curveTo(76.89f, 27.95f, 64.38f, 30.45f, 64.38f, 30.45f)
+                lineTo(66.57f, 37.37f)
+                curveTo(66.57f, 37.37f, 69.58f, 36.77f, 70.12f, 39.48f)
+                curveTo(70.71f, 42.46f, 66.93f, 43.21f, 66.93f, 43.21f)
+                lineTo(67.85f, 53.21f)
+                lineTo(96.39f, 47.51f)
+                curveTo(96.39f, 47.51f, 107.37f, 40.67f, 91.96f, 34.24f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFBEBEBE))) {
-                moveTo(33.7f, 68.4f)
-                lineTo(28.2f, 29.68f)
-                curveTo(28.2f, 29.68f, 27.9f, 27.72f, 30.17f, 27.43f)
-                curveTo(32.81f, 27.08f, 32.83f, 28.67f, 32.83f, 28.67f)
-                lineTo(40.31f, 67.18f)
-                curveTo(40.31f, 67.18f, 39.64f, 68.97f, 37.61f, 69.59f)
-                curveTo(34.96f, 70.39f, 33.7f, 68.4f, 33.7f, 68.4f)
+                moveTo(51.7f, 92.4f)
+                lineTo(46.2f, 53.68f)
+                curveTo(46.2f, 53.68f, 45.9f, 51.72f, 48.17f, 51.43f)
+                curveTo(50.81f, 51.08f, 50.83f, 52.67f, 50.83f, 52.67f)
+                lineTo(58.31f, 91.18f)
+                curveTo(58.31f, 91.18f, 57.64f, 92.97f, 55.61f, 93.59f)
+                curveTo(52.96f, 94.39f, 51.7f, 92.4f, 51.7f, 92.4f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFBEBEBE))) {
-                moveTo(87.55f, 52.57f)
-                lineTo(71.51f, 22.51f)
-                curveTo(71.51f, 22.51f, 71.09f, 20.92f, 72.75f, 20.02f)
-                curveTo(74.58f, 19.01f, 76.27f, 20.93f, 76.27f, 20.93f)
-                lineTo(92.49f, 50.08f)
-                curveTo(92.49f, 50.08f, 92.4f, 51.66f, 90.94f, 52.63f)
-                curveTo(89.03f, 53.88f, 87.55f, 52.57f, 87.55f, 52.57f)
+                moveTo(105.55f, 76.57f)
+                lineTo(89.51f, 46.51f)
+                curveTo(89.51f, 46.51f, 89.09f, 44.92f, 90.75f, 44.02f)
+                curveTo(92.58f, 43.01f, 94.27f, 44.93f, 94.27f, 44.93f)
+                lineTo(110.49f, 74.08f)
+                curveTo(110.49f, 74.08f, 110.4f, 75.66f, 108.93f, 76.63f)
+                curveTo(107.03f, 77.88f, 105.55f, 76.57f, 105.55f, 76.57f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFD9D9D9))) {
-                moveTo(98.28f, 64.68f)
-                lineTo(33.61f, 91.91f)
-                curveTo(33.61f, 91.91f, 24.52f, 94.51f, 20.92f, 84.91f)
-                curveTo(17.33f, 75.31f, 25.92f, 70f, 25.92f, 70f)
-                lineTo(92.48f, 47.64f)
-                curveTo(92.48f, 47.64f, 98.31f, 46.83f, 100.7f, 53.15f)
-                curveTo(103.41f, 60.34f, 98.28f, 64.68f, 98.28f, 64.68f)
+                moveTo(116.28f, 88.68f)
+                lineTo(51.61f, 115.91f)
+                curveTo(51.61f, 115.91f, 42.52f, 118.5f, 38.92f, 108.91f)
+                curveTo(35.33f, 99.31f, 43.92f, 94f, 43.92f, 94f)
+                lineTo(110.48f, 71.64f)
+                curveTo(110.48f, 71.64f, 116.31f, 70.83f, 118.7f, 77.15f)
+                curveTo(121.41f, 84.34f, 116.28f, 88.68f, 116.28f, 88.68f)
                 close()
             }
             path(
@@ -123,25 +123,25 @@ public val CloseGripHandle: ImageVector
                 stroke = SolidColor(Color(0xFFAAAAAA)),
                 strokeLineWidth = 1f
             ) {
-                moveTo(46.82f, 85.3f)
-                curveTo(49.39f, 78.09f, 45.27f, 66.77f, 40.68f, 65.29f)
-                lineTo(82.47f, 51.57f)
-                curveTo(88.84f, 54.31f, 91.36f, 59.96f, 90.19f, 67.43f)
-                lineTo(46.82f, 85.3f)
+                moveTo(64.82f, 109.3f)
+                curveTo(67.39f, 102.09f, 63.27f, 90.77f, 58.68f, 89.29f)
+                lineTo(100.47f, 75.57f)
+                curveTo(106.84f, 78.31f, 109.36f, 83.96f, 108.19f, 91.43f)
+                lineTo(64.82f, 109.3f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFA0A0A0))) {
-                moveTo(34.35f, 77.31f)
-                curveTo(37.41f, 81.89f, 37.37f, 87.29f, 34.26f, 89.38f)
-                curveTo(31.14f, 91.46f, 26.13f, 89.43f, 23.07f, 84.85f)
-                curveTo(20f, 80.27f, 21.11f, 75.84f, 24.23f, 73.76f)
-                curveTo(27.34f, 71.68f, 31.29f, 72.73f, 34.35f, 77.31f)
+                moveTo(52.35f, 101.31f)
+                curveTo(55.41f, 105.89f, 55.37f, 111.29f, 52.26f, 113.38f)
+                curveTo(49.14f, 115.46f, 44.13f, 113.43f, 41.07f, 108.85f)
+                curveTo(38f, 104.27f, 39.11f, 99.84f, 42.23f, 97.76f)
+                curveTo(45.34f, 95.68f, 49.29f, 96.73f, 52.35f, 101.31f)
                 close()
             }
         }.build()
 
-        return _Closegriphandle!!
+        return _CloseGripHandle!!
     }
 
 @Suppress("ObjectPropertyName")
-private var _Closegriphandle: ImageVector? = null
+private var _CloseGripHandle: ImageVector? = null
--- compose-libs/equipments/src/commonMain/kotlin/equipments/WideGripHandle.kt
+++ compose-libs/equipments/src/commonMain/kotlin/equipments/WideGripHandle.kt
@@ -8,26 +8,26 @@ import androidx.compose.ui.unit.dp
 
 public val WideGripHandle: ImageVector
     get() {
-        if (_Widegriphandle != null) {
-            return _Widegriphandle!!
+        if (_WideGripHandle != null) {
+            return _WideGripHandle!!
         }
-        _Widegriphandle = ImageVector.Builder(
-            name = "Widegriphandle",
-            defaultWidth = 264.dp,
-            defaultHeight = 169.dp,
-            viewportWidth = 264f,
-            viewportHeight = 169f
+        _WideGripHandle = ImageVector.Builder(
+            name = "WideGripHandle",
+            defaultWidth = 279.dp,
+            defaultHeight = 181.dp,
+            viewportWidth = 279f,
+            viewportHeight = 181f
         ).apply {
             path(fill = SolidColor(Color(0xFFD9D9D9))) {
-                moveTo(37.62f, 8f)
-                lineTo(236.63f, 124.5f)
-                lineTo(257.13f, 162.5f)
-                curveTo(256.96f, 165.83f, 250.6f, 170.1f, 245f, 166.5f)
-                lineTo(229f, 135.46f)
-                lineTo(29.41f, 19.35f)
-                lineTo(3f, 23.5f)
-                curveTo(0.5f, 19.5f, 0.37f, 15f, 3f, 12.5f)
-                lineTo(37.62f, 8f)
+                moveTo(46.63f, 13f)
+                lineTo(245.63f, 129.5f)
+                lineTo(268.5f, 172f)
+                curveTo(268.33f, 175.33f, 261.98f, 179.6f, 256.38f, 176f)
+                lineTo(238f, 140.45f)
+                lineTo(41.5f, 25.5f)
+                lineTo(5.5f, 30f)
+                curveTo(3f, 26f, 2.87f, 20.5f, 5.5f, 18f)
+                lineTo(46.63f, 13f)
                 close()
             }
             path(
@@ -35,11 +35,11 @@ public val WideGripHandle: ImageVector
                 stroke = SolidColor(Color(0xFFAAAAAA)),
                 strokeLineWidth = 1f
             ) {
-                moveTo(64.5f, 39f)
-                curveTo(63.87f, 34.77f, 69.54f, 29.67f, 71.5f, 28.5f)
-                lineTo(98.5f, 44.5f)
-                curveTo(94.89f, 45.75f, 92.19f, 52.58f, 92f, 55f)
-                lineTo(64.5f, 39f)
+                moveTo(53.5f, 32f)
+                curveTo(52.87f, 27.77f, 58.54f, 22.67f, 60.5f, 21.5f)
+                lineTo(107.5f, 49.5f)
+                curveTo(103.89f, 50.75f, 101.2f, 57.58f, 101f, 60f)
+                lineTo(53.5f, 32f)
                 close()
             }
             path(
@@ -47,11 +47,11 @@ public val WideGripHandle: ImageVector
                 stroke = SolidColor(Color(0xFFAAAAAA)),
                 strokeLineWidth = 1f
             ) {
-                moveTo(31.5f, 20f)
-                curveTo(30.87f, 15.77f, 35.54f, 9.67f, 37.5f, 8.5f)
-                lineTo(63f, 23.5f)
-                curveTo(59.39f, 24.75f, 56.19f, 31.58f, 56f, 34f)
-                lineTo(31.5f, 20f)
+                moveTo(5.91f, 29.5f)
+                curveTo(2.43f, 27.02f, 4.04f, 19.83f, 5.91f, 18.6f)
+                lineTo(38.5f, 14.5f)
+                curveTo(36.91f, 17.97f, 37.89f, 23.18f, 39.5f, 25f)
+                lineTo(5.91f, 29.5f)
                 close()
             }
             path(
@@ -59,11 +59,11 @@ public val WideGripHandle: ImageVector
                 stroke = SolidColor(Color(0xFFAAAAAA)),
                 strokeLineWidth = 1f
             ) {
-                moveTo(6f, 22.5f)
-                curveTo(2.82f, 19.63f, 4.24f, 15.15f, 5f, 13f)
-                lineTo(28.5f, 10f)
-                curveTo(26.52f, 13.26f, 25.61f, 17.01f, 27f, 19f)
-                lineTo(6f, 22.5f)
+                moveTo(174f, 102.5f)
+                curveTo(173.37f, 98.27f, 178.04f, 93.17f, 180f, 92f)
+                lineTo(231.5f, 122f)
+                curveTo(227.9f, 123.25f, 225.2f, 129.58f, 225f, 132f)
+                lineTo(174f, 102.5f)
                 close()
             }
             path(
@@ -71,65 +71,41 @@ public val WideGripHandle: ImageVector
                 stroke = SolidColor(Color(0xFFAAAAAA)),
                 strokeLineWidth = 1f
             ) {
-                moveTo(165f, 97.5f)
-                curveTo(164.37f, 93.27f, 169.04f, 88.17f, 171f, 87f)
-                lineTo(198.5f, 103f)
-                curveTo(194.89f, 104.25f, 192.19f, 110.58f, 192f, 113f)
-                lineTo(165f, 97.5f)
-                close()
-            }
-            path(
-                fill = SolidColor(Color(0xFF424346)),
-                stroke = SolidColor(Color(0xFFAAAAAA)),
-                strokeLineWidth = 1f
-            ) {
-                moveTo(201.5f, 118.5f)
-                curveTo(200.87f, 114.27f, 206.04f, 109.68f, 208f, 108.5f)
-                lineTo(235f, 124f)
-                curveTo(231.39f, 125.25f, 228.2f, 131.58f, 228f, 134f)
-                lineTo(201.5f, 118.5f)
-                close()
-            }
-            path(
-                fill = SolidColor(Color(0xFF424346)),
-                stroke = SolidColor(Color(0xFFAAAAAA)),
-                strokeLineWidth = 1f
-            ) {
-                moveTo(255.5f, 160.5f)
-                curveTo(253.84f, 164.44f, 246.78f, 164.51f, 244.5f, 164.5f)
-                lineTo(232f, 140f)
-                curveTo(235.73f, 140.79f, 240.08f, 136.96f, 241.5f, 135f)
-                lineTo(255.5f, 160.5f)
+                moveTo(268.5f, 172.5f)
+                curveTo(266.84f, 176.45f, 261f, 177.5f, 257.5f, 176.5f)
+                lineTo(241f, 145f)
+                curveTo(244.73f, 145.79f, 249.08f, 141.96f, 250.5f, 140f)
+                lineTo(268.5f, 172.5f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFA0A0A0))) {
-                moveTo(108.5f, 64.39f)
-                curveTo(108.9f, 61.19f, 113.46f, 55.72f, 115.12f, 54.39f)
-                curveTo(119.52f, 53.59f, 129.12f, 58.39f, 132.62f, 60.39f)
-                curveTo(130.62f, 51.89f, 136.92f, 42.72f, 144.12f, 43.89f)
-                curveTo(147.68f, 44.47f, 146.12f, 50.39f, 146.12f, 50.39f)
-                curveTo(135.62f, 44.39f, 134.62f, 55.89f, 138.12f, 57.39f)
-                lineTo(137.62f, 64.39f)
-                curveTo(135.62f, 65.19f, 144f, 82.5f, 143f, 85f)
-                curveTo(131.33f, 79.33f, 108.1f, 67.59f, 108.5f, 64.39f)
+                moveTo(117.5f, 69.39f)
+                curveTo(117.9f, 66.19f, 122.46f, 60.72f, 124.13f, 59.39f)
+                curveTo(128.52f, 58.59f, 138.13f, 63.39f, 141.63f, 65.39f)
+                curveTo(139.63f, 56.89f, 145.92f, 47.72f, 153.13f, 48.89f)
+                curveTo(156.68f, 49.47f, 155.13f, 55.39f, 155.13f, 55.39f)
+                curveTo(144.63f, 49.39f, 143.63f, 60.89f, 147.13f, 62.39f)
+                lineTo(146.63f, 69.39f)
+                curveTo(144.63f, 70.19f, 153f, 87.5f, 152f, 90f)
+                curveTo(140.33f, 84.33f, 117.1f, 72.59f, 117.5f, 69.39f)
                 close()
             }
             path(fill = SolidColor(Color(0xFFA0A0A0))) {
-                moveTo(149f, 87.5f)
-                curveTo(150f, 84.57f, 151.62f, 78.89f, 154.12f, 77.39f)
-                curveTo(152.58f, 73.19f, 142.94f, 66.51f, 140.12f, 63.89f)
-                curveTo(148.62f, 59.89f, 151.12f, 45.39f, 144.12f, 43.89f)
-                curveTo(142.06f, 43.45f, 141.12f, 48.39f, 141.12f, 48.39f)
-                curveTo(148.62f, 50.89f, 142.12f, 59.89f, 138.12f, 57.39f)
-                curveTo(133.94f, 54.78f, 131.12f, 67.89f, 131.12f, 67.89f)
-                lineTo(126.62f, 74.39f)
-                curveTo(125.5f, 76f, 147.96f, 90.55f, 149f, 87.5f)
+                moveTo(158f, 92.5f)
+                curveTo(159f, 89.57f, 160.63f, 83.89f, 163.12f, 82.39f)
+                curveTo(161.58f, 78.19f, 151.94f, 71.51f, 149.12f, 68.89f)
+                curveTo(157.62f, 64.89f, 160.13f, 50.39f, 153.13f, 48.89f)
+                curveTo(151.06f, 48.45f, 150.12f, 53.39f, 150.12f, 53.39f)
+                curveTo(157.63f, 55.89f, 151.12f, 64.89f, 147.12f, 62.39f)
+                curveTo(142.94f, 59.78f, 140.12f, 72.89f, 140.12f, 72.89f)
+                lineTo(135.62f, 79.39f)
+                curveTo(134.5f, 81f, 156.96f, 95.55f, 158f, 92.5f)
                 close()
             }
         }.build()
 
-        return _Widegriphandle!!
+        return _WideGripHandle!!
     }
 
 @Suppress("ObjectPropertyName")
-private var _Widegriphandle: ImageVector? = null
+private var _WideGripHandle: ImageVector? = null
